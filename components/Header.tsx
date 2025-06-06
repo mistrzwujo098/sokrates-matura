6cb34ae4f873c124140621894b2d1bc08fd10264
mistrzwujo098 <kacperczaczyk@gmail.com>
2025-06-06 16:09:46 +0200

    Fix: Add missing AnimatePresence import in Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import { useTheme } from './ThemeProvider'
 import { Sun, Moon, Brain, Menu, X } from 'lucide-react'
 import { useState } from 'react'
